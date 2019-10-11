1563dbcfd316f890a7fa8b71401115fad850fec1
Sandra <sandraromanrivera@hotmail.com>
2019-10-11 13:17:57 -0500

    Fixed run_image.sh, changed index.html and added styles

--- run_image.sh
+++ run_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --name myContainer --interactive --tty --rm --publish 8080:4200 --volume ${PWD}:/usr/src/app angular-docker-image
+sudo docker run --name myContainer --interactive --tty --rm --publish 8080:4200 --volume "${PWD}/my-angular-site:/usr/src/app" angular-docker-image
